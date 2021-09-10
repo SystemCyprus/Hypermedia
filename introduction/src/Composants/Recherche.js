db9338cb4a1c7feb36483c01ed3720c679b9aa4a
c64ren <nicolaeflorin@protonmail.com>
2021-09-10 17:06:56 -0400

    Teste .gitignore et node-module et fixe le probleme app.css et semantic-ui-css

---
+++ introduction/src/Composants/Recherche.js
@@ -0,0 +1,50 @@
+import { Component } from 'react';
+import { Button, Select } from 'semantic-ui-react';
+import './Recherche.css';
+
+class Recherche extends Component {
+
+    state = {
+        dpt: '',
+        type: ''
+    }
+
+    onDptChange = (e, data) => {
+        this.setState({ dpt: data.value })
+    }
+
+    onTypeChange = (e, data) => {
+        this.setState({ type: data.value })
+    }
+
+
+
+    render() {
+        const optionsDpt = [
+            { value: "44", key: "44", text: "Loire Atlantique" },
+            { value: "49", key: "49", text: "Maine et Loire" },
+            { value: "53", key: "53", text: "Mayenne" },
+            { value: "72", key: "72", text: "Sarthe" },
+            { value: "85", key: "85", text: "Vendée" },
+        ];
+
+
+        const optionsType = [
+            { value: "cpam", key: "cpam", text: "Caisse primaire d'assurance maladie" },
+            { value: "cci", key: "cci", text: "Chambre de commerce et d'industrie" },
+            { value: "crous", key: "crous", text: "Crous et ses antennes" }
+        ]
+
+
+        return (
+            <div className="recherche">
+                <Select placeholder="Choisissez un département" options={optionsDpt} value={this.state.dpt} c={this.onDptChange} />
+                <Select placeholder="Choisissez une administration" options={optionsType} value={this.state.type} c={this.onTypeChange} />
+                <Button primary c={() => this.props.onHaythem(this.state.dpt, this.state.type)}> Lancer la recherche </Button>
+                <Button secondary> Vider la recherche </Button>
+
+            </div>
+        )
+    }
+}
+export default Recherche;
